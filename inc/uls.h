da44fa1f83b70644bb14fd44403c9d658c371687
asamson <Freefire25>
2020-01-22 23:32:09 +0200

    update project

--- inc/uls.h
+++ inc/uls.h
@@ -3,14 +3,25 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <malloc/malloc.h>
+#include <pwd.h>
+#include <grp.h>
 
-
+void do_ls(char[]);
+void dostat(char *);
+void show_file_info( char *, struct stat*);
+void mode_to_letters(int, char[]);
+char *uid_to_name(uid_t);
+char *gid_to_name(gid_t);
+int mx_strcmp(const char *s1, const char *s2);
+char *mx_strcpy(char *dst, const char *src);
 
 
 
